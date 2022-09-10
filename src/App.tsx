c8ab5aacf649977ad5f01208bc8e3b61cd74163d
SKOLZ <gabriel.zanzotti@gmail.com>
2022-09-10 16:29:33 +0200

    created base structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Contact from './Screens/Contact';
+import Location from './Screens/Location';
+import News from './Screens/News';
+import Photos from './Screens/Photos';
+import Project from './Screens/Project';
+import Logo from './assets/logo.png';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className="main">
+      <img src={Logo} alt="" className="logo" />
+      <nav className="navbar">
+        <NavLink className="nav-link" to="/">Novedades</NavLink>
+        <NavLink className="nav-link" to="/project">Proyecto</NavLink>
+        <NavLink className="nav-link" to="/location">Ubicacion</NavLink>
+        <NavLink className="nav-link" to="/photos">Fotos</NavLink>
+        <NavLink className="nav-link" to="/contact">Contacto</NavLink>
+      </nav>
+        <article>
+          <Routes>
+            <Route path="/" element={<News />} />
+            <Route path="project" element={<Project />} />
+            <Route path="location" element={<Location />} />
+            <Route path="photos" element={<Photos />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+        </article>
+    </main>
   )
 }
 
