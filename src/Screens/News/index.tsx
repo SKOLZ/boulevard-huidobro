08af03f41889d7da8bbbe7fe318d7e30c51b27e8
SKOLZ <gabriel.zanzotti@gmail.com>
2022-09-10 23:31:06 +0200

    finished project view.

--- src/Screens/News/index.tsx
+++ src/Screens/News/index.tsx
@@ -3,9 +3,9 @@ import "./styles.scss";
 
 function News() {
   return (
-    <article className="content">
+    <article className="news-content">
       <YoutubeEmbed embedId="KHkydFOHmzY" className="video" />
-      <hr className="separator"/>
+      <hr className="news-separator"/>
       <h2 className="news-title">Avance de obra - Noviembre</h2>
       <YoutubeEmbed embedId="YDBkowXh78I" className="video" />
       <h2 className="news-title">Avance de obra - Agosto</h2>
